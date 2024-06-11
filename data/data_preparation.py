ff6ca9b98bb275bf422487974ab2eb9879be2df2
hieuadoan <hieu.a.doan@gmail.com>
2024-06-10 21:38:31 -0500

    Removed unnecessary import

--- data/data_preparation.py
+++ data/data_preparation.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from pymatgen.core import Structure
 from pymatgen.io.cif import CifParser
 import ase.io
 import yaml
